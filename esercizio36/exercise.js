9198032f052b77ef8b4443b04f643e13e388affd
DavideCatanzaro <dadocata03@gmail.com>
2023-04-04 12:43:45 +0200

    done

--- esercizio36/exercise.js
+++ esercizio36/exercise.js
@@ -1,13 +1,11 @@
-function repeatHello(callback){
-   let repeat = setInterval(callback, 1000);
-   setTimeout(() => clearInterval(repeat), 5000);
-    
+function repeatHello(callback) {
+  let repeat = setInterval(callback, 1000);
+  setTimeout(() => clearInterval(repeat), 5000);
 }
 
-function hello(){
-    console.log("HELLO")
-}
+let hello = () => {
+  console.log("HELLO");
+};
 
 repeatHello(hello);
-
-
+//usiamo l'arrow function in questo caso perchè il codice è più leggibile e sopratutto perchè l'arrow function ha il return implicito
