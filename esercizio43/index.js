a51b87e52373f826dc83ad3206bd82f90a34efe1
DavideCatanzaro <dadocata03@gmail.com>
2023-04-14 11:20:17 +0200

    done

---
+++ esercizio43/index.js
@@ -0,0 +1,14 @@
+const user = {
+    id: 1,
+    name: "John",
+    age: 25,
+  };
+  
+  
+  function loadStor() {
+    const userlocal = JSON.stringify(user)
+    localStorage.setItem("user", userlocal);
+    console.log(userlocal);
+  }
+  
+  loadStor();
